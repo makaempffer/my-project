4fc37d48fb0498a8969e771eac9634a0077cd0af
makaempffer <ma.kaempffer@duocuc.cl>
2022-11-17 22:24:42 -0300

    Fixed mobile Cards display and added Menu Icon

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,10 +4,12 @@ import casa2 from '../assets/house_card_example_2.png';
 
 export default function Cards() {
     return (
-        <div className='p-14 md:flex container space-x-10 w-screen mx-auto'>
-            <div className='w-1/2 md:w-80 rounded-lg md-h-1/2 opacity-50 hover:opacity-100'>
-                <div className='pt-5 px-5 pb-2 flex shrink-0 justify-center items-center'>
-                    <button className='absolute text-white text-3xl blur hover:blur-none'>+</button>
+        <div className='p-5 md:p-14 md:flex container md:space-x-10 w-screen mx-auto'>
+            <div className='w-fit md:w-80 rounded-lg md-h-1/2 md:opacity-50 hover:opacity-100'>
+                <div className='pt-5 px-5 pb-2 flex shrink-0 items-center justify-center'>
+                    <div className='absolute flex items-center justify-center hover:blur-none md:blur'>
+                        <button className='absolute text-white text-4xl'>+</button>
+                    </div>
                     <img className='rounded-xl' src={casa}></img>  
                 </div>
                 <div className='px-5'>
@@ -24,8 +26,11 @@ export default function Cards() {
                 </div>
             </div>
 
-            <div className='w-1/2 md:w-80 rounded-lg md-h-1/2 opacity-50 hover:opacity-100'>
-                <div className='pt-5 px-5 pb-2 shrink-0'>
+            <div className='w-fit md:w-80 rounded-lg md-h-1/2 md:opacity-50 hover:opacity-100'>
+                <div className='pt-5 px-5 pb-2 flex shrink-0 items-center justify-center'>
+                    <div className='absolute flex items-center justify-center hover:blur-none md:blur'>
+                        <button className='absolute text-white text-4xl'>+</button>
+                    </div>
                     <img className='rounded-xl' src={casa2}></img>  
                 </div>
                 <div className='px-5'>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
-
+import Menu from '../assets/menu_icon.png';
 export default function NavBar() {
     return (
       <nav className="relative container mx-auto p-6 border-b border-veryLightBlue">
         <div className="flex items-center justify-between">
           <a href="/"><h1 className="font-roboto font-bold text-3xl text-white py-2 md:pl-40 tracking-wider">Loren Ipsum.</h1></a>
+          <button><img className='md:hidden active:rotate-45' src={Menu}></img></button>
           <div className="hidden md:flex space-x-24 text-white pr-40 font-outfit">
               <a className='hover:text-slate-500' href="/">Home</a>
               <a className='hover:text-slate-500' href="/projects">Projects</a>
